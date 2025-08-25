5fbe7ead29171c1b1a09ddccbe21561bf7c989c5
Vinoothna28 <23jr1a05g0@gmail.com>
2025-08-25 15:45:05 +0530

    updating api for ai

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 def generateAI():
     dataset = pd.read_csv('data.csv')
     dataset = dataset.dropna()
 
-    
-    X = dataset.iloc[:, 0].values.reshape(-1, 1)  
-    y = dataset.iloc[:, 1].values                  
+ 
+    X = dataset.iloc[:, 0].values.reshape(-1, 1)
+    y = dataset.iloc[:, 1].values
 
-   from sklearn.model_selection import train_test_split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-   from sklearn.neighbors import KNeighborsClassifier
     ai = KNeighborsClassifier(n_neighbors=5)
     ai.fit(X_train, y_train)
 
-    import pickle
     pickle.dump(ai, open('model.pkl', 'wb'))
